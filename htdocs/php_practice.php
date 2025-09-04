4c38cebec5b812033984871f7d27bd144919bd8b
shiontkhs <chrsrivera15@gmail.com>
2025-09-04 20:02:14 +0800

    Add files via upload

--- htdocs/php_practice.php
+++ htdocs/php_practice.php
@@ -25,8 +25,8 @@
     <?php 
         //echo "Hello";
 
-        $color = "Red";
-        $Color = "Blue";
+        //$color = "Red";
+        //$Color = "Blue";
         //echo $color;
         //echo $Color;
 
@@ -38,7 +38,7 @@
         //var_dump(false);
         //var_dump([1,2]);
 
-        $x = 5;
+        //$x = 5;
 
         //function test(){
             //$x = 10;
@@ -60,7 +60,7 @@
         //$x = print("Hello");
         //echo $x;
 
-        // $a = "Chan";
+        //$a = "Chan";
         // $b = 5;
         // $c = 5.9;
         // $d = true;
@@ -100,13 +100,121 @@
         //echo "Hello World!";
         //echo "<br>";
 
-        $x = "Hello";
-        $y = "Love!";
+        // $x = "Hello";
+        // $y = "Love!";
         
-        echo $x . " " . $y, "<br>";
-        echo "$x dgfdgfsg $y";
-        echo '<br>';
-        echo "Juan \"Immortal\" Enrile"; //error
+        // echo $x . " " . $y, "<br>";
+        // echo "$x dgfdgfsg $y";
+        // echo '<br>';
+        // echo "Juan \"Immortal\" Enrile"; //error
+
+
+        // $a = (String) "Chan";
+        // $b = (String) 5;
+        // $c = (String) 5.9;
+        // $d = (String) true;
+        // $e = (String) [1,2,3];
+        // $g = (String) null;  
+        // $h = (String) fopen("php_practice.php", "r");
+        
+        // unsent($a);
+
+        // var_dump($a);
+        // ECHO "<br>";
+        // var_dump($b);
+        // ECHO "<br>";
+        // var_dump($c);
+        // ECHO "<br>";
+        // var_dump($d);
+        // ECHO "<br>";
+        // var_dump($e);
+        // ECHO "<br>";
+        // var_dump($g);
+        // ECHO "<br>";
+        // var_dump($h);
+        // ECHO "<br>";
+
+        // echo pi();
+
+        // echo abs(6);
+
+        // echo sqrt(2);
+
+        //echo round(5.5);
+
+        //echo rand(5, 20);
+
+        //constants
+
+        // $_asd = 5;
+        // define("GREETING", 'HELLo');
+        // echo GREETING;
+
+        // function test() {
+        //     echo GREETING;
+        // }
+
+        // test();
+
+        //$X = 5;
+
+        // +=
+        //$x = $x + 10;
+        // $x += 10;
+
+        //echo $x;
+
+        // $x %=5;
+
+        // echo $x;
+
+        // var_dump(5 == 5); //truenes
+        // var_dump("5" === "5"); //truenes
+        // var_dump(5 <> 7); //truenes
+        // var_dump(5 !== "5"); //truenes
+        // var_dump();
+
+        //echo 3 <=> 2;
+
+        //increment ++
+
+        // pre-increment/decrement
+
+        // $x = 5;
+        // echo ++$x;
+
+        //post-increment/decrement
+        // $y = 5;
+        // echo $y++;
+        // echo $y;
+
+        //logical
+
+        // and && 
+        // var_dump(true and true); // both true
+        // var_dump(5=="5" && 5==5); // both true
+
+        // or ||
+        //var_dump(false or false);
+
+        // xor
+        //var_dump(true xor true); // only one true
+
+        // ! reverse 
+
+       // var_dump(!true);
+
+
+       //string operators
+       //.
+       //$x = "Hello" . "World";
+
+       //.= append
+       //$x .= "World!";
+       //echo $x;
+
+
+
 
     ?>
 </body>
